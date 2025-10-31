7eef92fceed1f91ad71ffd65eb8cefb4a3945ca0
Romain Wintgens <rwintgen@student.42madrid.com>
2025-10-31 16:53:44 +0100

    Modified scritps to take several arguments from Command Line

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-java -cp src io.github.rwintgen.avaj_launcher.Main 
+ARG="${1:-scenario.txt}"
+
+java -cp src io.github.rwintgen.avaj_launcher.Main $1
--- scripts/silent_full_run.sh
+++ scripts/silent_full_run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+ARG="${1:-scenario.txt}"
+
 bash scripts/compile.sh > /dev/null
-bash scripts/run.sh > /dev/null
+bash scripts/run.sh "$ARG" > /dev/null
 bash scripts/clean.sh > /dev/null
