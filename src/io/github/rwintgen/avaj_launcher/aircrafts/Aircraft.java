06e98e3d3d4e56456774e33418b095c58813d482
Romain Wintgens <rwintgen@student.42madrid.com>
2025-11-03 19:27:30 +0100

    Implemented Aircraft and fixed Flyable (Untested)

--- src/io/github/rwintgen/avaj_launcher/aircrafts/Aircraft.java
+++ src/io/github/rwintgen/avaj_launcher/aircrafts/Aircraft.java
@@ -1,1 +1,30 @@
 package io.github.rwintgen.avaj_launcher.aircrafts;
+
+import io.github.rwintgen.avaj_launcher.utils.Coordinates;
+import io.github.rwintgen.avaj_launcher.buildings.WeatherTower;
+
+abstract class Aircraft implements Flyable {
+
+    protected final long id;
+    protected final String name;
+    protected Coordinates coordinates;
+
+    abstract void updateConditions() {
+    }
+
+    abstract void registerTower(WeatherTower p_tower) {
+    }
+
+    protected Aircraft(long p_id, String p_name, Coordinates p_coordinates) {
+        if (p_id < 0) {
+            throw new IllegalArgumentException("Aircraft id must be a positive integer (current: " + p_id + ").");
+        }
+        if (p_name == null || p_name.empty()) {
+            throw new IllegalArgumentException("Aircraft name cannot be empty.");
+        }
+
+        id = p_id;
+        name = p_name;
+        coordinates = p_coordinates;
+    }
+}
--- src/io/github/rwintgen/avaj_launcher/aircrafts/Flyable.java
+++ src/io/github/rwintgen/avaj_launcher/aircrafts/Flyable.java
@@ -4,6 +4,6 @@ import io.github.rwintgen.avaj_launcher.weather.WeatherTower;
 
 public interface Flyable {
 
-    void updateConditions();
-    void registerTower(WeatherTower p_tower);
+    public void updateConditions();
+    public void registerTower(WeatherTower p_tower);
 }
